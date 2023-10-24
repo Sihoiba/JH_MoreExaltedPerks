89ff05577d3bdb29316abf30f844834fbd4fb98d
Sihoiba <dominic.decesare+github@gmail.com>
2023-10-24 23:17:12 +0100

    Gatekeeper locked elevators have a clear visual glow

--- gfx.lua
+++ gfx.lua
@@ -149,3 +149,21 @@ register_gfx_blueprint "power_up_100" {
         range       = 2.0,
     }
 }
+
+register_gfx_blueprint "mod_exalted_gatekeeper_elevator_inactive"
+{
+	equip = {},
+	uisprite = {
+		icon = "data/texture/ui/icons/ui_tooltip_elevator",
+		orientation = "FLOOR",
+		offset = 1.5,
+		propagate = true,
+		color = vec4(1.5,0.75,2.25,1.0),
+		visibility = "REVEAL",
+	},
+	light = {
+		position    = vec3(0.5,0,0),
+		color       = vec4(1.5,0.75,2.25,2.0),
+		range       = 2.5,
+	},
+}
